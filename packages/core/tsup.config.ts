681edba4e7b2e13eab9b6272f1fa73be7c6d326f
Apzoniqx <asingh@zoniqx.com>
2026-04-19 01:56:13 +0530

    perf: disable bundling for smaller bundle sizes and better tree-shaking

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/**/*.ts"],
   format: ["cjs", "esm"],
   dts: false,
   clean: true,
   sourcemap: true,
+  bundle: false,
   external: [],
 });
